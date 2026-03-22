0751065e4701b8a1518e416f3730b0569105afb3
AlesyaBorisova <alesya.yunussova91@gmail.com>
2026-03-22 10:35:06 +0300

    Update CourierClient.java

--- src/test/java/service/CourierClient.java
+++ src/test/java/service/CourierClient.java
@@ -1,5 +1,6 @@
 package service;
 
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import model.Courier;
@@ -10,6 +11,8 @@ public class CourierClient {
     private static final String CREATE_ENDPOINT = "/api/v1/courier";
     private static final String LOGIN_ENDPOINT = "/api/v1/courier/login";
     private static final String DELETE_ENDPOINT = "/api/v1/courier/:id";
+
+    @Step("Создать курьера с логином: {courier.login}")
     public Response create(Courier courier) {
         return RestAssured.given()
                 .header("Content-Type", "application/json")
@@ -18,6 +21,7 @@ public class CourierClient {
                 .post(CREATE_ENDPOINT);
     }
 
+    @Step("Войти под курьером с логином: {courierCreds.login}")
     public Response login(CourierCreds courierCreds) {
         return RestAssured.given()
                 .header("Content-Type", "application/json")
@@ -26,6 +30,7 @@ public class CourierClient {
                 .post(LOGIN_ENDPOINT);
     }
 
+    @Step("Удалить курьера с ID: {courierId}")
     public Response delete(int courierId) {
         return RestAssured.given()
                 .when()
